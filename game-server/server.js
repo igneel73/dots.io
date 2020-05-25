6ce77bb68ae3b774da5a20861c6aad48272cec53
Kush Parmar <igneel.kush73@gmail.com>
2020-05-24 20:19:30 -0700

    made simple chat server

--- game-server/server.js
+++ game-server/server.js
@@ -4,12 +4,22 @@
  */
 
 const app = require('express')();
+const http = require('http').createServer(app);
 const config = require('./config/config');
+const io = require('socket.io')(http);
 
 // Route Definitions
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/static/html/index.html'));
+
+// Socket Definitions
+io.on('connection', (socket) => {
+  console.log('A User Connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 // Run server
-app.listen(config.PORT, () =>
+http.listen(config.PORT, () =>
   console.log(`Example app listening at http://localhost:${config.PORT}`)
 );
